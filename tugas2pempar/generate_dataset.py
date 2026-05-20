25abff9339318263a99dbd6ee8b53291305fc2a7
Arthur <plamlagi123@gmail.com>
2026-05-20 10:34:08 +0800

    fixbuatpaper

--- tugas2pempar/generate_dataset.py
+++ tugas2pempar/generate_dataset.py
@@ -2,13 +2,13 @@ import pandas as pd
 import random
 
 # Load dataset asli
-df = pd.read_csv("spam.csv", encoding='latin-1')
+df = pd.read_csv("spam_10000.csv", encoding='latin-1')
 
 # Ambil hanya kolom penting
 df = df[['v1', 'v2']]
 
 # Target jumlah data
-targets = [10000, 25000, 50000, 75000, 100000]
+targets = [10000, 25000, 50000, 75000, 100000, ]
 
 for target in targets:
     # Random duplicate rows sampai mencapai target
